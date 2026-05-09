1104ce8071ed45e07d3eb4b0a843337db79c7cba
cut-the-freakin-steak <calvaradolopez@proton.me>
2026-05-09 09:51:56 -0600

    maybe

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,19 @@ fn main() -> AppExit {
 }
 
 #[derive(Component)]
-enum Direction {
+enum ArrowDirection {
     Left,
     Right,
+    Up,
+    Down,
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
 
-    commands.spawn((Sprite::from_image(asset_server.load(""))));
+    commands.spawn((
+        Sprite::from_image(asset_server.load("sonic.webp")),
+        ArrowDirection::Up,
+        Transform::from_xyz(0.0, 0.0, 0.0),
+    ));
 }
